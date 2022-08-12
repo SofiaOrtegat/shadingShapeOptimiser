764f67d4b7dd5052b52a307b8a77293b3cc965a9
SofiaOrtegat <SofiaOrtegat@proton.me>
2022-08-12 14:19:57 +0000

    Updating generated files

--- Code/GCMex_test.m
+++ Code/GCMex_test.m
@@ -13,4 +13,5 @@ for row = 0:H-1
     pixel = 1+ row*W + col;
     if row+1 < H, pairwise(pixel, 1+col+(row+1)*W) = 1; end
     if row-1 >= 0, pairwise(pixel, 1+col+(row-1)*W) = 1; end 
-    if col+1 < W, pairwise(pixel, 1+(col+1)+
+    if col+1 < W, pairwise(pixel, 1+(col+1)+row*W) = 1; end
+    if col-1 >= 
