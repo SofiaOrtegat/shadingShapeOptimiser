a7e84eb26cbb1412195ababd4904231ad446945f
SofiaOrtegat <SofiaOrtegat@proton.me>
2022-09-14 18:58:59 +0000

    [demos]

--- Code/diffuseLight.m
+++ Code/diffuseLight.m
@@ -1,4 +1,5 @@
 function lightImg = diffuseLight(surfaceNormal)
 [m, n, ~] = size(surfaceNormal);
 lightImg = zeros(m, n);
-light = [-2,2,2]/sqrt(14)
+light = [-2,2,2]/sqrt(14);
+normal = zeros(3,1);
