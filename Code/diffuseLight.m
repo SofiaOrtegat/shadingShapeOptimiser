b3e5bec1a9ab2d4c89a3f61f3e9e230ab2c64643
SofiaOrtegat <SofiaOrtegat@proton.me>
2022-09-21 14:40:31 +0000

    Adding .travis.yml

--- Code/diffuseLight.m
+++ Code/diffuseLight.m
@@ -5,4 +5,5 @@ light = [-2,2,2]/sqrt(14);
 normal = zeros(3,1);
 for i = 1:m
     for j = 1:n
-        normal(:,1) = surfaceNormal(i, j, :
+        normal(:,1) = surfaceNormal(i, j, :);
+        lightImg(
