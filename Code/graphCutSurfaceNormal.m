bb76234d12ca0ab98ae0272303b107104aa8d2c7
SofiaOrtegat <SofiaOrtegat@proton.me>
2022-09-27 20:28:04 +0000

    Assignment 3

--- Code/graphCutSurfaceNormal.m
+++ Code/graphCutSurfaceNormal.m
@@ -9,3 +9,28 @@ epsilon = 0.01;
 [W, H, ~] = size(surfaceNormal);
 Icosahedron = icosahedron(0.1);
 [sIco, ~] = size(Icosahedron);
+
+NormalLabel = zeros(W, H);
+for i = 1:W
+    for j = 1:H
+        d = (Icosahedron(:,1)-surfaceNormal(i,j,1)).^2 + ...
+            (Icosahedron(:,2)-surfaceNormal(i,j,2)).^2 + ...
+            (Icosahedron(:,3)-surfaceNormal(i,j,3)).^2;
+        [~, index] = min(d);
+        NormalLabel(i, j) = index;
+    end
+end
+
+WH = W*H;
+segclass = reshape(NormalLabel, 1, [])';  % initial Normal
+pairwise = sparse(WH,WH);                 % neighborhood term 
+unary = zeros(sIco,WH);                   % cost connect to each label
+labelcost = zeros(sIco, sIco);            % smoothness term
+
+for row = 0:H-1
+  for col = 0:W-1
+    pixel = 1+ row*W + col;
+    if row+1 < H 
+        Na = segclass(pixel);
+        Nb = segclass(1+col+(row+1)*W);
+        ico = Icosahedron(Na, :) - Icosahedron(Nb, :);
