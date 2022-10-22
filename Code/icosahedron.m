2e67aa9f9d6389ed4344dd2213b58f060de28bca
SofiaOrtegat <SofiaOrtegat@proton.me>
2022-10-22 21:25:22 +0000

    Add grunt

--- Code/icosahedron.m
+++ Code/icosahedron.m
@@ -1,3 +1,4 @@
 function output = icosahedron(subdivider) % subdivide surface of icosahedron, smaller -> finer
 t = (1+sqrt(5))/2; %golden ratio
-a = sqrt(t)/5^(1/4)
+a = sqrt(t)/5^(1/4);
+b = 1/(sqrt(t)*5
