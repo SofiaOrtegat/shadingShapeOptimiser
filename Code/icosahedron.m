58b674ad51582eca590db89287acb439f7ead637
SofiaOrtegat <SofiaOrtegat@proton.me>
2022-10-25 15:43:26 +0000

    dm thin: reduce endio_hook pool size

--- Code/icosahedron.m
+++ Code/icosahedron.m
@@ -2,4 +2,6 @@ function output = icosahedron(subdivider) % subdivide surface of icosahedron, sm
 t = (1+sqrt(5))/2; %golden ratio
 a = sqrt(t)/5^(1/4);
 b = 1/(sqrt(t)*5^(1/4));
-c = a+2*b
+c = a+2*b;
+d = a+b;
+x =  [0;0;0;0;b;-b
