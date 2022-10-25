3bdc91ef3cc5a9adc08016258b7e2a6afc76a97b
SofiaOrtegat <SofiaOrtegat@proton.me>
2022-10-25 13:26:05 +0000

    Address review

--- Code/icosahedron.m
+++ Code/icosahedron.m
@@ -1,4 +1,5 @@
 function output = icosahedron(subdivider) % subdivide surface of icosahedron, smaller -> finer
 t = (1+sqrt(5))/2; %golden ratio
 a = sqrt(t)/5^(1/4);
-b = 1/(sqrt(t)*5
+b = 1/(sqrt(t)*5^(1/4));
+c = a+2*b
