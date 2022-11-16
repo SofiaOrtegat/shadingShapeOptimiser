046fbe0f28cd2e70540e2e6bf4a3a3b3aa2f89ad
SofiaOrtegat <SofiaOrtegat@proton.me>
2022-11-16 14:50:40 +0000

    Added changelog entry.

--- Code/icosahedron.m
+++ Code/icosahedron.m
@@ -17,4 +17,4 @@ for m = 1:20
     d = (x-mx(m)).^2 + (y-my(m)).^2 + (z-mz(m)).^2;
     [~, order] = sort(d);
     p1 = [mx(m), my(m), mz(m)];
-    p2 = [x(order
+    p2 = [x(order(1)), y(order(1)), z(order(
