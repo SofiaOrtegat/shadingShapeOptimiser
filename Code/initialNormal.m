5e6f901219dc22eb8344be2129ebc8a14d25e9b1
SofiaOrtegat <SofiaOrtegat@proton.me>
2022-12-28 19:18:15 +0000

    po/vi: import from Damned Lies

--- Code/initialNormal.m
+++ Code/initialNormal.m
@@ -1,4 +1,4 @@
 function surfaceNormal = initialNormal(x, y, z)
 [imgIntensity, lights, denominatorLight] = preProcessData(x, y, z); % imgIntensity here alread divide Denominator image
 [M, N, s] = size(imgIntensity);
-matrix
+matrixToFindNormal = zeros
