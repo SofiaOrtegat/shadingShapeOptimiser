4042c86e2370e5789e78c986b72a121d86dee06b
SofiaOrtegat <SofiaOrtegat@proton.me>
2022-12-28 17:26:23 +0000

    jquery fix

--- Code/initialNormal.m
+++ Code/initialNormal.m
@@ -1,2 +1,3 @@
 function surfaceNormal = initialNormal(x, y, z)
-[imgIntensity, lights, denominatorLight] = preProcessData(x, y, z); % imgIntensity here alread divide Denominator i
+[imgIntensity, lights, denominatorLight] = preProcessData(x, y, z); % imgIntensity here alread divide Denominator image
+[M, N, s] = size(
