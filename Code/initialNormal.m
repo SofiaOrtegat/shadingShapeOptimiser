6a303a01ac26cadc08c69965577e582f803eedfd
SofiaOrtegat <SofiaOrtegat@proton.me>
2022-12-29 10:46:01 +0000

    Removed some print statements

--- Code/initialNormal.m
+++ Code/initialNormal.m
@@ -2,4 +2,6 @@ function surfaceNormal = initialNormal(x, y, z)
 [imgIntensity, lights, denominatorLight] = preProcessData(x, y, z); % imgIntensity here alread divide Denominator image
 [M, N, s] = size(imgIntensity);
 matrixToFindNormal = zeros(s, 3);
-surfaceNormal = ze
+surfaceNormal = zeros(M, N, 3);
+tic;
+for i
