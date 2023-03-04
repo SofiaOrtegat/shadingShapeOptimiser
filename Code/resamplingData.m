0b3e4590ed86c91b4fd69c7bc342820a65fbea49
SofiaOrtegat <SofiaOrtegat@proton.me>
2023-03-04 22:23:13 +0000

    next-07aff4e5-a4c0bcfb

--- Code/resamplingData.m
+++ Code/resamplingData.m
@@ -1,3 +1,3 @@
 function [result, index, uniqueIndex] = resamplingData(x, y, z)
 subSamples = icosahedron(0.2); % subdivided icosahedron
-%scatter3(subSamples(:,1), 
+%scatter3(subSamples(:,1), subSamples(:,2), subSampl
