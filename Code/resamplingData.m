f8a1db0502137f4263b5eeefca90da10dced3507
SofiaOrtegat <SofiaOrtegat@proton.me>
2023-03-07 06:48:22 +0000

    ARM: 7660/1: tlb: add branch predictor maintenance operations

--- Code/resamplingData.m
+++ Code/resamplingData.m
@@ -1,4 +1,5 @@
 function [result, index, uniqueIndex] = resamplingData(x, y, z)
 subSamples = icosahedron(0.2); % subdivided icosahedron
 %scatter3(subSamples(:,1), subSamples(:,2), subSamples(:,3));
-s = size(sub
+s = size(subSamples, 1);
+result = 
