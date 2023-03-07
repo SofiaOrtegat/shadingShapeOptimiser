78c10fc8103bb9436441428f82bb299607a16c99
SofiaOrtegat <SofiaOrtegat@proton.me>
2023-03-07 15:27:18 +0000

    table style

--- Code/resamplingData.m
+++ Code/resamplingData.m
@@ -2,4 +2,6 @@ function [result, index, uniqueIndex] = resamplingData(x, y, z)
 subSamples = icosahedron(0.2); % subdivided icosahedron
 %scatter3(subSamples(:,1), subSamples(:,2), subSamples(:,3));
 s = size(subSamples, 1);
-result = 
+result = [];
+index = [];
+for i = 1
