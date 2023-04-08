9f16fcc647225b63bc81214152905c9237835760
SofiaOrtegat <SofiaOrtegat@proton.me>
2023-04-08 19:22:46 +0000

    Bump to 0.4

--- Code/subdivide.m
+++ Code/subdivide.m
@@ -2,4 +2,4 @@ function result = subdivide(p1, p2, p3, subdivider)
 result = [];
     for i = subdivider:subdivider:1
         for j = 0:1/(1/subdivider-(1-i)/subdivider):1
-            result = [re
+            result = [result; j*(i*p1+(1-i)*p2) +
