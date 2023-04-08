fdcb353225b09fe88f4825302dc93b711d1647ae
SofiaOrtegat <SofiaOrtegat@proton.me>
2023-04-08 22:33:40 +0000

    Improved example

--- Code/subdivide.m
+++ Code/subdivide.m
@@ -3,4 +3,6 @@ result = [];
     for i = subdivider:subdivider:1
         for j = 0:1/(1/subdivider-(1-i)/subdivider):1
             result = [result; j*(i*p1+(1-i)*p2) + (1-j)*(i*p3+(1-i)*p2)];
- 
+        end
+    end
+    res
