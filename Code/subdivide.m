8521464ef2eb30be7af908ebd8a465f750010dba
SofiaOrtegat <SofiaOrtegat@proton.me>
2023-04-08 17:37:49 +0000

    NR-474: Refactor the collaborators setup in the blog configuration screen

--- Code/subdivide.m
+++ Code/subdivide.m
@@ -1,4 +1,5 @@
 function result = subdivide(p1, p2, p3, subdivider)
 result = [];
     for i = subdivider:subdivider:1
-        for j = 0:1/(1/subdivider-(1-i)/subdiv
+        for j = 0:1/(1/subdivider-(1-i)/subdivider):1
+            result = [re
